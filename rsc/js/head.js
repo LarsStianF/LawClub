2f313af45d4dbc6200770ea29990cb25ca808cb4
Lars Stian <larsstianf@outlook.com>
2018-02-05 22:07:07 +0100

    Fikset bootstrap og importsetninger. (se rsc/js/head.js for å se hvordan vi løser <head> tag på alle sidene

---
+++ rsc/js/head.js
@@ -0,0 +1,33 @@
+document.write(
+
+    '<!-- Generell metadata: Start -->' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+    '<meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+    '<!-- Generell metadata: Slutt -->' +
+
+    '<!-- jQuery import: Start -->' +
+    // '<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>' +
+    '<!-- jQuery import: End -->' +
+
+    '<!-- CSS import: Start -->' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/bootstrap.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/main.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/material-icons.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/mobile.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/bootstrap.min.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/bootstrap-responsive.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/bootstrap-responsive.min.css">' +
+
+    '<link rel="stylesheet" type="text/css" href="rsc/css/pages/agenda.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/pages/index.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/pages/lawyers.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/pages/login.css">' +
+    '<link rel="stylesheet" type="text/css" href="rsc/css/pages/question.css">' +
+    '<!-- CSS import: Slutt -->' +
+
+    '<!-- JS import: Start -->' +
+    '<script src="rsc/js/bootstrap.js"></script>' +
+    '<script src="rsc/js/bootstrap.min.js"></script>' +
+    '<!-- JS import: End -->'
+
+);
